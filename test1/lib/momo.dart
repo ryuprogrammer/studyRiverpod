1dccadc48b4c5a4d81c2c17c2a71b5cdff9e44b7
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-04 00:59:37 +0900

    make freezed file

--- test1/lib/momo.dart
+++ test1/lib/momo.dart
@@ -1,11 +1,19 @@
 import 'dart:async';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 part 'momo.g.dart';
 
 @riverpod
 class S4Notifier extends _$S4Notifier {
   @override
-  Stream<String> build() {}
+  Stream<String> build() {
+    // supabaseのnotesテーブルにアクセスするためのストリーム
+    // データベースのリアルタイム変更を取得できる
+    final notesStream =
+        Supabase.instance.client.from('notes').stream(primaryKey: ['id']);
+
+    // return notesStream;
+  }
 
   // データを変更する関数
   void updateState() async {}
