097fced3ca21fb34632decef706f9e8eb9ea9601
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-04 01:27:50 +0900

    to fix

--- test1/lib/notes.dart
+++ test1/lib/notes.dart
@@ -1,1 +1,29 @@
 // freezedを使用して、ファイル生成するお
+
+///////// a. パッケージをインポートし、自動生成されるファイルを記述 /////////
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+// 生成されるdartファイルを記述
+// Freezedで生成されるデータクラス
+part 'notes.freezed.dart';
+// jsonを変換する処理が生成されるクラス
+// part 'notes.g.dart';
+
+///////// b. コード生成するためのクラスを作成 /////////
+
+// freezedでコード生成するために「@freezed」を記述
+@freezed
+class Notes with _$Notes {
+  // withの後には「_$[class name]」の形式で記述
+
+  // プロパティを指定
+  const factory Note({
+    required int id,
+    required String createAt,
+    required String todoItem,
+    required String isDone,
+  }) = _Note;
+
+  // json形式で受け取るためのコードを生成するために記述
+  // factory Note.fromJson(Map<String, dynamic> json) => _$NoteFromJson(json);
+}
